57c62007573a2516e0bd80f24c8429a84ec9660a
teera12 <bbkonkayeva@gmail.com>
2022-11-23 14:54:19 +0600

    Initial Commit

---
+++ ChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by Bibigul Konkayeva on 23.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
